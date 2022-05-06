52ec50bdb401a725225a9c98cf196f4ef040b843
charlene-johnson <charlene.j8234@gmail.com>
2022-05-06 19:44:23 -0400

    finished the contact us page, responsive design as well

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import MobileApps from "./MobileApps";
 import Websites from "./Websites";
 import Revolution from "./Revolution";
 import About from "./About";
+import Contact from "./Contact";
 
 function App() {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -105,7 +106,13 @@ function App() {
           <Route
             exact
             path="/contact"
-            component={() => <div>Contact Us</div>}
+            render={(props) => (
+              <Contact
+                {...props}
+                setValue={setValue}
+                setSelectedIndex={setSelectedIndex}
+              />
+            )}
           />
           <Route exact path="/estimate" component={() => <div>Estimate</div>} />
         </Switch>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,82 +1,104 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 
 const arcBlue = "#0B72B9"
 const arcOrange = "#FFBA60"
 const arcGrey="#868686"
 
-export default createMuiTheme({
-    palette: {
-        common: {
-            blue: arcBlue,
-            orange: arcOrange
-        },
-        primary: {
-            main: arcBlue
-        },
-        secondary: {
-            main: arcOrange
-        }
+export default createTheme({
+  palette: {
+    common: {
+      blue: arcBlue,
+      orange: arcOrange,
     },
-    typography: {
-        tab: {
-            fontFamily: 'Raleway',
-            textTransform: "none",
-            fontWeight: 700,
-            fontSize: "1rem",
-            color: "white"
+    primary: {
+      main: arcBlue,
+    },
+    secondary: {
+      main: arcOrange,
+    },
+  },
+  typography: {
+    tab: {
+      fontFamily: "Raleway",
+      textTransform: "none",
+      fontWeight: 700,
+      fontSize: "1rem",
+      color: "white",
+    },
+    h2: {
+      fontFamily: "Raleway",
+      fontWeight: 700,
+      fontSize: "2.5rem",
+      color: arcBlue,
+      lineHeight: 1.5,
+    },
+    h3: {
+      fontFamily: "Pacifico",
+      fontSize: "2.5rem",
+      color: arcBlue,
+    },
+    h4: {
+      fontFamily: "Raleway",
+      fontSize: "1.75rem",
+      color: arcBlue,
+      fontWeight: 700,
+    },
+    subtitle1: {
+      fontSize: "1.25rem",
+      fontWeight: 300,
+      color: arcGrey,
+    },
+    subtitle2: {
+      color: "white",
+      fontSize: "1.25rem",
+      fontWeight: 300,
+    },
+    body1: {
+      fontSize: "1.25rem",
+      color: arcGrey,
+      fontWeight: 300,
+    },
+    learnButton: {
+      borderColor: arcBlue,
+      color: arcBlue,
+      borderWidth: 2,
+      textTransform: "none",
+      borderRadius: 50,
+      fontFamily: "Roboto",
+      fontWeight: "bold",
+    },
+    estimate: {
+      fontFamily: "Pacifico",
+      fontSize: "1rem",
+      textTransform: "none",
+      color: "white",
+    },
+    caption: {
+      fontSize: "1rem",
+      fontWeight: 300,
+      color: arcGrey,
+    },
+  },
+  overrides: {
+    MuiInputLabel: {
+      root: {
+        color: arcBlue,
+        fontSize: "1rem",
+      },
+    },
+    MuiInput: {
+      root: {
+        color: arcGrey,
+        fontWeight: 300
+      },
+      underline: {
+        "&:before": {
+          borderBottom: `2px solid ${arcBlue}`,
         },
-        h2: {
-            fontFamily: "Raleway",
-            fontWeight: 700,
-            fontSize: "2.5rem",
-            color: arcBlue,
-            lineHeight: 1.5
+        "&:hover:not($disabled):not($focused):not($error):before": {
+          borderBottom: `2px solid ${arcBlue}`,
         },
-        h3: {
-            fontFamily:"Pacifico",
-            fontSize: "2.5rem",
-            color: arcBlue,
-        },
-        h4: {
-          fontFamily: "Raleway",
-          fontSize: "1.75rem",
-          color: arcBlue,
-          fontWeight: 700  
-        },
-        subtitle1: {
-            fontSize:"1.25rem",
-            fontWeight:300,
-            color: arcGrey,
-        },
-        subtitle2: {
-            color: "white",
-            fontSize: "1.25rem",
-            fontWeight: 300
-        },
-        body1: {
-            fontSize: "1.25rem",
-            color: arcGrey,
-            fontWeight: 300
-        },
-        learnButton: {
-        borderColor:arcBlue,
-        color:arcBlue,
-        borderWidth: 2,
-        textTransform: "none",
-        borderRadius: 50,
-        fontFamily: "Roboto",
-        fontWeight: "bold"
-        },
-        estimate: {
-            fontFamily: "Pacifico",
-            fontSize: "1rem",
-            textTransform: "none",
-            color: "white"
-        },
-        caption: {
-            fontSize: "1rem",
-            fontWeight: 300,
-            color: arcGrey
-        },
-    }
+      },
+    },
+  },
 });
